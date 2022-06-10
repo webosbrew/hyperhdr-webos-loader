d35ba3306ae37d94ee85198dcc44f5e5ace1350d
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-06-10 02:50:23 +0200

    feat: Spawn process via posix_spawn

--- service/src/service.h
+++ service/src/service.h
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <unistd.h>
 #include <glib.h>
+#include <pthread.h>
 
 #define SERVICE_NAME "org.webosbrew.hyperhdr.loader.service"
 #define HYPERHDR_PATH "/media/developer/apps/usr/palm/services/org.webosbrew.hyperhdr.loader.service/hyperhdr"
 
 typedef struct {
-    bool running;
     char *hyperhdr_version;
+    pid_t daemon_pid;
+    pthread_t execution_thread;
 } service_t;
 
 int service_start(service_t* service);
