337a75ee4ebd49d3fc0e160c7865a335ae762dd6
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-06-14 23:26:20 +0200

    autostart: Elevate service in autostart script

--- service/scripts/autostart.sh
+++ service/scripts/autostart.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-# Start HyperHDR
-luna-send -n 1 'luna://org.webosbrew.hyperhdr.loader.service/start' '{}'
+SERVICE_NAME="org.webosbrew.hyperhdr.loader.service"
+ELEVATION_SCRIPT="/media/developer/apps/usr/palm/services/org.webosbrew.hbchannel.service/elevate-service"
+
+# If elevation script is available, execute it for good measure
+if [ -f ${ELEVATION_SCRIPT} ]
+then
+    ${ELEVATION_SCRIPT} ${SERVICE_NAME}
+fi
+
+# Start daemon
+luna-send -n 1 "luna://${SERVICE_NAME}/start" '{}' &
