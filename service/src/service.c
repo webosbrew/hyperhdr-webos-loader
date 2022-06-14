c670ec90ea1d993b04d6891a43140dd9885866e5
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-06-14 06:26:38 +0200

    native service: Split into a few source files

---
+++ service/src/service.c
@@ -0,0 +1,168 @@
+#include <stdbool.h>
+#include <pbnjson.h>
+#include "service.h"
+#include "daemon.h"
+#include "log.h"
+
+bool service_method_start(LSHandle* sh, LSMessage* msg, void* data)
+{
+    service_t* service = (service_t*)data;
+    LSError lserror;
+    LSErrorInit(&lserror);
+
+    jvalue_ref jobj = jobject_create();
+    int res = daemon_start(service);
+
+    jobject_set(jobj, j_cstr_to_buffer("returnValue"), jboolean_create(res == 0));
+    switch (res) {
+        case 0:
+            jobject_set(jobj, j_cstr_to_buffer("status"), jstring_create("Daemon started successfully"));
+            break;
+        case 1:
+            jobject_set(jobj, j_cstr_to_buffer("status"), jstring_create("Precondition fail: Not running elevated!"));
+            break;
+        case 2:
+            jobject_set(jobj, j_cstr_to_buffer("status"), jstring_create("Daemon is already running"));
+            break;
+        case 3:
+            jobject_set(jobj, j_cstr_to_buffer("status"), jstring_create("Daemon failed to start, posix_spawn failed"));
+            break;
+        case 4:
+            jobject_set(jobj, j_cstr_to_buffer("status"), jstring_create("Daemon failed to start, pthread_create failed"));
+            break;
+        default:
+            jobject_set(jobj, j_cstr_to_buffer("status"), jstring_create("Daemon failed to start, reason: Unknown"));
+    }
+    LSMessageReply(sh, msg, jvalue_tostring_simple(jobj), &lserror);
+
+    j_release(&jobj);
+
+    return true;
+}
+
+bool service_method_stop(LSHandle* sh, LSMessage* msg, void* data)
+{
+    service_t* service = (service_t*)data;
+    LSError lserror;
+    LSErrorInit(&lserror);
+
+    jvalue_ref jobj = jobject_create();
+    int res = daemon_stop(service);
+    jobject_set(jobj, j_cstr_to_buffer("returnValue"), jboolean_create(res == 0));
+    switch (res) {
+        case 0:
+            jobject_set(jobj, j_cstr_to_buffer("status"), jstring_create("Daemon stopped successfully"));
+            break;
+        case 1:
+            jobject_set(jobj, j_cstr_to_buffer("status"), jstring_create("Precondition fail: Not running elevated!"));
+            break;
+        case 2:
+            jobject_set(jobj, j_cstr_to_buffer("status"), jstring_create("Daemon is not running"));
+            break;
+        default:
+            jobject_set(jobj, j_cstr_to_buffer("status"), jstring_create("Daemon failed to stop, reason: Unknown"));
+    }
+    LSMessageReply(sh, msg, jvalue_tostring_simple(jobj), &lserror);
+
+    j_release(&jobj);
+
+    return true;
+}
+
+bool service_method_version(LSHandle* sh, LSMessage* msg, void* data)
+{
+    service_t* service = (service_t*)data;
+    LSError lserror;
+    LSErrorInit(&lserror);
+
+    jvalue_ref jobj = jobject_create();
+    int res = daemon_version(service);
+    jobject_set(jobj, j_cstr_to_buffer("returnValue"), jboolean_create(res == 0));
+    jobject_set(jobj, j_cstr_to_buffer("returnCode"), jnumber_create_i32(res));
+    switch (res) {
+        case 0:
+            jobject_set(jobj, j_cstr_to_buffer("version"), jstring_create(service->daemon_version));
+            break;
+        default:
+            jobject_set(jobj, j_cstr_to_buffer("version"), jstring_create("Error while fetching"));
+            break;
+    }
+    LSMessageReply(sh, msg, jvalue_tostring_simple(jobj), &lserror);
+
+    j_release(&jobj);
+
+    return true;
+}
+
+bool service_method_status(LSHandle* sh, LSMessage* msg, void* data)
+{
+    service_t* service = (service_t*)data;
+    LSError lserror;
+    LSErrorInit(&lserror);
+
+    jvalue_ref jobj = jobject_create();
+    jobject_set(jobj, j_cstr_to_buffer("returnValue"), jboolean_create(true));
+    jobject_set(jobj, j_cstr_to_buffer("running"), jboolean_create(is_running(service->daemon_pid)));
+    jobject_set(jobj, j_cstr_to_buffer("elevated"), jboolean_create(is_elevated()));
+    jobject_set(jobj, j_cstr_to_buffer("pid"), jnumber_create_i32(service->daemon_pid));
+    jobject_set(jobj, j_cstr_to_buffer("daemon"), jstring_create(DAEMON_NAME));
+
+    LSMessageReply(sh, msg, jvalue_tostring_simple(jobj), &lserror);
+
+    j_release(&jobj);
+
+    return true;
+}
+
+bool service_method_terminate(LSHandle* sh, LSMessage* msg, void* data)
+{
+    service_t* service = (service_t*)data;
+    LSError lserror;
+    LSErrorInit(&lserror);
+
+    WARN("Terminating");
+
+    int res = daemon_terminate(service);
+
+    jvalue_ref jobj = jobject_create();
+    jobject_set(jobj, j_cstr_to_buffer("returnValue"), jboolean_create(res == 0));
+
+    LSMessageReply(sh, msg, jvalue_tostring_simple(jobj), &lserror);
+
+    j_release(&jobj);
+
+    if (res == 0) {
+        // Stopping mainloop!
+        g_main_loop_quit(gmainLoop);
+    } else {
+        ERR("Failed to terminate self, daemon was not able to stop");
+    }
+
+    return true;
+}
+
+LSMethod methods[] = {
+    {"start", service_method_start, LUNA_METHOD_FLAGS_NONE },
+    {"stop", service_method_stop, LUNA_METHOD_FLAGS_NONE },
+    {"status", service_method_status, LUNA_METHOD_FLAGS_NONE },
+    {"version", service_method_version, LUNA_METHOD_FLAGS_NONE },
+    {"terminate", service_method_terminate, LUNA_METHOD_FLAGS_NONE },
+    { 0, 0, 0 }
+};
+
+bool service_init(LSHandle *handle, service_t *service, LSError *lserror)
+{
+    bool ret = false;
+
+    if ((ret = LSRegisterCategory(handle, "/", methods, NULL, NULL, lserror)) && !ret ) {
+        ERR("Unable to register category on Luna bus: %s", lserror->message);
+        return false;
+    }
+
+    if ((ret = LSCategorySetData(handle, "/", service, lserror)) && !ret ) {
+        ERR("Unable to set category data on Luna bus: %s", lserror->message);
+        return false;
+    }
+
+    return true;
+}
--- service/src/service.h
+++ service/src/service.h
@@ -3,12 +3,16 @@
 #include <unistd.h>
 #include <glib.h>
 #include <pthread.h>
+#include <luna-service2/lunaservice.h>
 
 #define LOG_NAME "hyperhdr-loader"
 #define SERVICE_NAME "org.webosbrew.hyperhdr.loader.service"
 #define DAEMON_PATH "/media/developer/apps/usr/palm/services/org.webosbrew.hyperhdr.loader.service/hyperhdr"
 #define DAEMON_EXECUTABLE "hyperhdr"
 #define DAEMON_NAME "HyperHDR"
+
+// Global from main.c
+extern GMainLoop *gmainLoop;
 
 typedef struct {
     char *daemon_version;
@@ -18,3 +22,4 @@ typedef struct {
 
 int service_start(service_t* service);
 int service_stop(service_t* service);
+bool service_init(LSHandle *handle, service_t *service, LSError *lserror);
